8c56062fa8223bf8a2f43d157805ebd42457f546
MrPekar98 <martin.pekar@outlook.dk>
2021-03-17 15:46:14 +0100

    Automated download of data.

---
+++ download.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/circulosmeos/gdown.pl.git
+mkdir train
+
+cd train
+./../gdown.pl/gdown.pl https://drive.google.com/file/d/1CScamR90Oc47nn7T8a7lFt14BRdLJYIN/view train.zip
+
+sudo apt install unzip -y
+unzip train.zip
+cd ..
+
+rm -rf gdown.pl
